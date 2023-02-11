e2233a8f28eec94b572142213016960339450993
Mikołaj Graczyk <mikolajgraczyk123@gmail.com>
2023-02-11 20:21:23 +0100

    fixed fetch error

--- src/features/Portfolio/getReposFromAPI.js
+++ src/features/Portfolio/getReposFromAPI.js
@@ -1,5 +1,5 @@
 export const getReposFromAPI = async () => {
-    const response = await fetch("https://apios");
+    const response = await fetch("https://api.github.com/users/scudoo/repos");
 
     if(!response.ok){
         new Error(response.statusText);
