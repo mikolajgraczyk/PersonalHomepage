92e2220610a09f1154fc3369f9ffcd08e8bb5048
Mikołaj Graczyk <mikolajgraczyk123@gmail.com>
2023-02-16 13:53:06 +0100

    skills are saved as array

--- src/features/SkillsetList/skills.js
+++ src/features/SkillsetList/skills.js
@@ -1,35 +1,34 @@
 import { ListElement } from "./styled"
 
-export const currentSkillset =
-    <>
-        <ListElement>Semantic & accessible HTML</ListElement>
-        <ListElement>Responsive Web Design</ListElement>
-        <ListElement>Teamwork</ListElement>
-        <ListElement>Markdown</ListElement>
-        <ListElement>ImmutabiListElementty</ListElement>
-        <ListElement>CSS BEM convention</ListElement>
-        <ListElement>CSS Grid</ListElement>
-        <ListElement>CSS Flexbox</ListElement>
-        <ListElement>Redux (Toolkit)</ListElement>
-        <ListElement>Redux Saga</ListElement>
-        <ListElement>React Hooks</ListElement>
-        <ListElement>Error HandListElementng</ListElement>
-        <ListElement>Working with API (fetch, axios)</ListElement>
-        <ListElement>JavaSrcipt: ES6+</ListElement>
-        <ListElement>GitHub Pull Requests & Review</ListElement>
-        <ListElement>Trello</ListElement>
-        <ListElement>Scrum</ListElement>
-        <ListElement>NPM</ListElement>
-        <ListElement>React</ListElement>
-        <ListElement>Git</ListElement>
-    </>
+export const currentSkillset = [
+    { skill: "Semantic & accessible HTML" },
+    { skill: "Responsive Web Design" },
+    { skill: "Teamwork" },
+    { skill: "Markdown" },
+    { skill: "Immutability" },
+    { skill: "CSS BEM convention" },
+    { skill: "CSS BEM convention" },
+    { skill: "CSS Grid" },
+    { skill: "CSS Flexbox" },
+    { skill: "Redux (Toolkit)" },
+    { skill: "Redux Saga" },
+    { skill: "React Hooks" },
+    { skill: "Error Handling" },
+    { skill: "Working with API (fetch, axios)" },
+    { skill: "JavaSrcipt: ES6+" },
+    { skill: "GitHub Pull Requests & Review" },
+    { skill: "Trello" },
+    { skill: "Scrum" },
+    { skill: "NPM" },
+    { skill: "React" },
+    { skill: "Git" },
+];
 
-export const wantToLearn =
-    <>
-        <ListElement>React Context</ListElement>
-        <ListElement>Node.js</ListElement>
-        <ListElement>Cypress</ListElement>
-        <ListElement>Unit testing</ListElement>
-        <ListElement>TypeScript</ListElement>
-        <ListElement>JS classes</ListElement>
-    </>
+export const wantToLearn = [
+    { skill: "React Context" },
+    { skill: "Node.js" },
+    { skill: "Cypress" },
+    { skill: "Unit testing" },
+    { skill: "TypeScript" },
+    { skill: "JS classes" },
+]
