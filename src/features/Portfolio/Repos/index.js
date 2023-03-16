f9f06930287f6b46c38cadff180d614c212dfd96
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-03-17 00:29:33 +0100

    removed manually added repo

--- src/features/Portfolio/Repos/index.js
+++ src/features/Portfolio/Repos/index.js
@@ -16,40 +16,6 @@ const Repos = () => {
 
   return (
     <Wrapper>
-      <Repo darkModeEnabled={theme}>
-        <RepoName darkModeEnabled={theme}>movies-browser</RepoName>
-        <RepoDescription darkModeEnabled={theme}>
-          The Movies Browser is a platform that allows users to search and
-          browse movies, popular actors and actresses, as well as their
-          production history. The website was created by a team of three
-          individuals as part of a group project and was completed within three
-          weeks.
-        </RepoDescription>
-        <RepoAndDemo darkModeEnabled={theme}>
-          Demo:
-          <span>
-            <URL
-              href="https://marcin10lw.github.io/movies-browser/#/movies"
-              target="_blank"
-              rel="noreferrer"
-              darkModeEnabled={theme}
-            >
-              https://marcin10lw.github.io/movies-browser/#/movies
-            </URL>
-          </span>
-          Code:
-          <span>
-            <URL
-              href="https://github.com/marcin10lw/movies-browser"
-              target="_blank"
-              rel="noreferrer"
-              darkModeEnabled={theme}
-            >
-              https://github.com/marcin10lw/movies-browser
-            </URL>
-          </span>
-        </RepoAndDemo>
-      </Repo>
       {repos.map((repo) => (
         <Repo key={repo.id} darkModeEnabled={theme}>
           <RepoName darkModeEnabled={theme}>{repo.name}</RepoName>
